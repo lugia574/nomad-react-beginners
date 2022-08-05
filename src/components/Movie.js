1ab44d59b26a73d29694bc4795987efbf8b9ced8
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-08-05 14:24:10 +0900

    2022_0805 test

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,7 +8,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
       <h2>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>아아아아{summary}</p>
+      <p>{summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
